c242012be9ac3e89b40aee67e58082f6bb60a61c
Thomas Peugnet <thomas.peugnet.pro@gmail.com>
2023-01-20 18:56:11 +0100

    Update setupTests.js

--- Client/src/setupTests.js
+++ Client/src/setupTests.js
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Write a simple react test
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+
+    // render() is a function from react-testing-library
+    // It renders the App component and returns a bunch of useful methods
+    // for making assertions about the component
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+
+});
