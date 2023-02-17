6c292a1ab8acc9b497212f8ef5e8b80895409280
Thomas Peugnet <97756028+DriftKing-DK@users.noreply.github.com>
2023-02-17 20:53:05 +0100

    Adding some things

--- Client/src/components/App.js
+++ Client/src/components/App.js
@@ -2,12 +2,14 @@ import logo from '../logo.svg';
 import '../styles/App.css';
 import './Window.js';
 import Window from "./Window";
+import Sidebar from "./Sidebar";
 
 import '../styles/window.css';
+import '../styles/sidebar.css';
 import '../styles/card.css';
 import '../styles/App.css';
 function App() {
-  return <div className="container"><Window /></div>
+  return <div className="container"><Sidebar /><Window /></div>
 }
 
 export default App;
---
+++ Client/src/components/Sidebar.js
@@ -0,0 +1,18 @@
+
+function Sidebar() {
+    return (
+            <div className="sideBar">
+                <div className="side-button">
+                    Home
+                </div>
+                <div className="side-button">
+                    Articles
+                </div>
+                <div className="side-button">
+                    More
+                </div>
+            </div>
+            )
+}
+
+export default Sidebar
