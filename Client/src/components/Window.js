cf313cdc171e243f96f975742b84c57f6d103abe
Thomas Peugnet <thomas.peugnet.dev@gmail.com>
2023-03-07 01:33:13 +0100

    API CALLING FOR ONE ARTICLE IS WORKIIIIIIIIIING

--- Client/src/components/Window.js
+++ Client/src/components/Window.js
@@ -1,6 +1,6 @@
 import Card from './Card'
 import Title from "./Title";
-import Article from "./Article";
+
 
 const lorem = "Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget ultricies nisl nisl eget nisl. eget ultricies nisl nisl eget nisl. eget ultricies nisl nisl eget nisl.Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl, eget ultricies nisl nisl eget nisl. eget ultricies nisl nisl eget nisl. eget ultricies nisl nisl eget nisl.";
 const lorem2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nisl nisl aliquet nisl";
@@ -10,54 +10,88 @@ const lorem4 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eui
 const title = "Treadstone";
 const subtitle = "A new kind of intelligence";
 
-function Window(props) {
+function Window(props)
+{
 
     const display_article = props.display_article;
     const article = props.article;
 
     /* use state to display article */
 
-    if (display_article) {
+    if (display_article)
+    {
         return display_article_f(article);
     }
 
-    else {
+    else
+    {
         return display_cards();
     }
 
-    function handleArticle(e) {
-        props.setArticle(e);
+    function handleArticle(e)
+    {
+        var article = null;
+        fetch("http://127.0.0.1:5000/articles/view/" + "coucou")
+            .then(response => response.json())
+            .then(data =>
+            {
+                article = data[0];
+                console.log(article)
+
+                // Change the state
+                props.setArticle(article);
+            });
+
         props.setDisplay_article(true);
     }
 
-    function handleGoBack(e) {
+    function handleGoBack(e)
+    {
         props.setArticle({});
         props.setDisplay_article(false);
     }
 
-    function display_article_f(e) {
-        console.log(e);
-        // do a get request to the api, to get the content of the article
-        return <div className="window-article">
-            <Article contentValue={e.description} titleValue={e.title} subtitleValue={e.subtitle} goBackFunction={handleGoBack} />
+
+    function display_article_f(e)
+    {
+        return <div className="window">
+            <Title titleValue={title} subtitleValue={subtitle}/>
+            <div className="article">
+                <div className="article-title">
+                    {article.title}
+                </div>
+                <div className="article-subtitle">
+                    {article.subtitle}
+                </div>
+                <div className="article-content">
+                    {article.content}
+                </div>
+            </div>
         </div>
     }
 
-    function display_cards() {
+    function display_cards()
+    {
         return <div className="window">
-            <Title titleValue={title} subtitleValue={subtitle} />
-            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem2} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem3} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem4} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem2} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem3} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem4} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
-            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle} />
+            <Title titleValue={title} subtitleValue={subtitle}/>
+            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem2} titleValue={title} subtitleValue={subtitle}
+                  onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem3} titleValue={title} subtitleValue={subtitle}
+                  onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem4} titleValue={title} subtitleValue={subtitle}
+                  onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem2} titleValue={title} subtitleValue={subtitle}
+                  onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem3} titleValue={title} subtitleValue={subtitle}
+                  onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem4} titleValue={title} subtitleValue={subtitle}
+                  onClickFunction={handleArticle}/>
+            <Card descriptionValue={lorem} titleValue={title} subtitleValue={subtitle} onClickFunction={handleArticle}/>
         </div>
     }
 
--- Server/src/endpoints/articles.py
+++ Server/src/endpoints/articles.py
@@ -7,9 +7,29 @@ route = Blueprint('route', __name__)
 @route.route('/', methods=['GET'])
 @route.route('/articles', methods=['GET'])
 def articles():
-    return jsonify({'articles': "coucou"}, 200)
+    lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc"
+    title = "Treadstone - Apised"
+    subtitle = "This article come from API"
+    # generate a random number of articles composed of a title, subtitle and a lorem, between 1 and 10
+    art_list = []
+    for i in range(1, 10):
+        art_list.append({
+            'title': title,
+            'subtitle': subtitle,
+            'content': lorem
+        })
+    return jsonify(art_list, 200)
 
 
 @route.route('/articles/view/<string:article_id>', methods=['GET'])
 def articles_view(article_id):
-    return jsonify({article_id: "coucou"}, 200)
+    # Generate a random article composed of a title, subtitle and a lorem
+    title = "Treadstone - Apised"
+    subtitle = "This article is specific"
+    lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vitae ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl nisl eu nunc. Nullam auctor, nisl eget ultricies lacinia, nunc nisl tincidunt nisl, eget aliquam nisl n"
+    return jsonify({
+        'title': title,
+        'subtitle': subtitle,
+        'content': lorem,
+        'id': article_id
+    }, 200)
